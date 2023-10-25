616c6bd211a3ab03b32a1f9fdd3e4bb40a57b3ff
rafaelcostafernandes <rafaelcostafernandes@yahoo.com>
2023-10-25 15:01:00 -0300

    alteração aula

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
